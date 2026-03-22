48d0ac6ea4f8e8ceae77f6d9914f2bb4bb1b8df3
midnightmunchies422-hash <midnightmunchies422@gmail.com>
2026-03-22 03:15:17 -0600

    Add order button for cookie orders

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 function order(item) {
   const phoneNumber = "16393995905"; // your real number
   const orderType = document.getElementById("orderType")?.value || "";
-
+<button onclick="window.location.href='sms:+16394709989?body=Hi%2C%20I%20want%20to%20order%20cookies'" class="order-button">
+  Order Now
+</button>
   let message = `Hi! I want to order: ${item}`;
 
   if (orderType) {
